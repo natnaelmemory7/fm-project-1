d4321c2fa21ddbf14be745947825568b2d3f41de
natnaelmemory7 <gua20202@gmail.com>
2025-04-19 03:01:48 +0300

    add all revised nav  with desktop first approach

--- build/js/nav.js
+++ build/js/nav.js
@@ -3,8 +3,10 @@
 let hamNav = document.querySelector('.ham--nav');
 let listNav = document.querySelector('.list--nav');
 let boxNav = document.querySelector('.box--nav');
+let imgList = document.querySelector('.img--list');
 
 boxNav.addEventListener("click", function(){
-    listNav.classList.toggle('is--hidden');
+    listNav.classList.toggle('is--active');
     hamNav.classList.toggle('is--active');
+    imgList.classList.toggle('is--active');
 })
